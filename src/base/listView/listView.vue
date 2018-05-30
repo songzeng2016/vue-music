f108415e3f6f87b3abc1ba6acececf81879300ec
songzeng <songzeng62@gmail.com>
2018-05-30 22:25:05 +0800

    完善歌手页面

--- src/base/listView/listView.vue
+++ src/base/listView/listView.vue
@@ -32,14 +32,22 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   import {getData} from 'common/js/dom'
 
   const ANCHOR_HEIGHT = 18
+  const TITLE_HEIGHT = 30
 
   export default {
     props: {
@@ -54,10 +62,12 @@
       return {
         scrollY: -1,
         currentIndex: 0,
+        diff: -1,
       }
     },
     components: {
       Scroll,
+      Loading,
     },
     created() {
       this.touch = {}
@@ -70,6 +80,12 @@
         return this.data.map((group) => {
           return group.title.substr(0, 1)
         })
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) {
+          return ''
+        }
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
       }
     },
     methods: {
@@ -132,7 +148,16 @@
         let preList = this.listHeight.filter(item => {
           return item <= newY
         })
+        this.diff = this.listHeight[preList.length] - newY
         this.currentIndex = preList.length - 1
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        if (this.fixedTop === fixedTop) {
+          return false
+        }
+        this.fixedTop = fixedTop
+        this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
       }
     }
   }
